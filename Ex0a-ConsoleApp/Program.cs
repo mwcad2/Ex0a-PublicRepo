18580625b45182bdf5ff902cfa6aebbcc7b27d4d
Mark Walsh <mwcad2@outlook.com>
2022-07-18 18:39:58 -0700

    add initial application

---
+++ Ex0a-ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex0_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type your PUBLIC name.");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
+            Console.ReadKey();
+        }
+    }
+}
